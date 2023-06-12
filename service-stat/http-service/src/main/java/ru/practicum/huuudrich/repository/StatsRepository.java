58baa3eb1c8f3da4b6000dea16174f8a52c46125
huuudrich <108597096+huuudrich@users.noreply.github.com>
2023-06-12 18:59:44 +1100

    create statistic module

--- service-stat/http-service/src/main/java/ru/practicum/huuudrich/repository/StatsRepository.java
+++ service-stat/http-service/src/main/java/ru/practicum/huuudrich/repository/StatsRepository.java
@@ -16,8 +16,18 @@ public interface StatsRepository extends JpaRepository<ServiceRequest, Long> {
             "AND :end AND sr.uri IN :uris GROUP BY sr.app, sr.uri")
     List<ShortStat> getUriHitCount(LocalDateTime start, LocalDateTime end, List<String> uris);
 
-    @Query("SELECT new ru.practicum.huuudrich.model.ShortStat(sr.app, sr.uri, COUNT(sr)) " +
+    @Query("SELECT new ru.practicum.huuudrich.model.ShortStat(sr.app, sr.uri, COUNT(DISTINCT sr.ip)) " +
             "FROM ServiceRequest sr WHERE sr.timestamp BETWEEN :start " +
             "AND :end AND sr.uri IN :uris GROUP BY sr.app, sr.uri")
     List<ShortStat> getUniqueUriHitCount(LocalDateTime start, LocalDateTime end, List<String> uris);
+
+    @Query("SELECT new ru.practicum.huuudrich.model.ShortStat(sr.app, sr.uri, COUNT(sr)) " +
+            "FROM ServiceRequest sr WHERE sr.timestamp BETWEEN :start " +
+            "AND :end GROUP BY sr.app, sr.uri")
+    List<ShortStat> getUriHitCountNotUris(LocalDateTime start, LocalDateTime end);
+
+    @Query("SELECT new ru.practicum.huuudrich.model.ShortStat(sr.app, sr.uri, COUNT(DISTINCT sr.ip)) " +
+            "FROM ServiceRequest sr WHERE sr.timestamp BETWEEN :start " +
+            "AND :end GROUP BY sr.app, sr.uri")
+    List<ShortStat> getUniqueUriHitCountNotUris(LocalDateTime start, LocalDateTime end);
 }
